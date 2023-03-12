8310ac0f43361fcfbb7edd752ba97cd95b7b3ca0
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2023-03-12 02:25:08 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { useState } from 'react';
-import OptionCol from './OptionCol';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function App() {
   const [options, setOptions] = useState([]);
@@ -15,19 +19,49 @@ function App() {
       addOption();
     }
   }
+  const [criteria, setCriteria] = useState([]);
+  const [ criteriaText, setCriteriaText ] = useState('');
+
+  const addCriteria = () => {
+    setCriteria([...criteria, criteriaText]);
+    setCriteriaText('');
+  }
+  const handlecriteriaTextChange = (event) => {
+    setCriteriaText(event.target.value);
+    if (event.key === 'Enter') {
+      addCriteria();
+    }
+  }
+
 
   return (
-    <div className="App">
+    <Container className="App">
       <h1>Decision helper</h1>
       <p>Please enter the title of the decision</p>
       <input className='title-input' type="text" placeholder='Which house should I rent? / Which candidate should I hire?' />
       <p>Please enter the options</p>
       <input className='option-input' type="text" placeholder='Option A' value={optionText} onKeyDown={handleOptionTextChange} onChange={handleOptionTextChange} />
       <button onClick={addOption} className='add-option-button'>Add option</button>
-      <div className='options'>
-        {options.map((option, index) => <OptionCol key={index} option={option} />)}
-      </div>
-    </div>
+      <p> Please enter a few criteria</p>
+      <input className='criteria-input' type="text" placeholder='Criteria 1' value={criteriaText} onKeyDown={handlecriteriaTextChange} onChange={handlecriteriaTextChange} />
+      <button className='add-criteria-button'>Add criteria</button>
+      <br />
+        <Row>
+          <Col className='criteria'>
+            <Row className='criteria-row'>Criteria</Row>
+            {criteria.map((criterion, index) => <Row key={index} className='criterion'><input type="text" value={criterion} /></Row>)}
+          </Col>
+          {options.map((option, index) => {
+            return (
+              <Col key={index} >
+                 <Row>{ option} </Row>
+                  {criteria.map((criterion, index) => <Row key={index} className='points'><input type="text" /></Row>)}
+              </Col>
+            )
+          })}
+        </Row>
+
+    </Container>
   );
 }
 
